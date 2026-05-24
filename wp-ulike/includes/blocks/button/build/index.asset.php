b9e8c7cf869ce192212e63decf7ecafc7ce13d8f
Sean Murphy <sean@iamseanmurphy.com>
2026-05-24 09:49:01 +0000

    5.0.4

--- wp-ulike/includes/blocks/button/build/index.asset.php
+++ wp-ulike/includes/blocks/button/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'dc77112b4c124afc6433');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '34b74ecb6791ca907251');
